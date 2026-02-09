820df65f18278505c30279b4a20c1a90cb8a2647
lakhansing patil <lakhannpatil2211@gmail.com>
2026-02-09 20:54:52 +0530

    Update firebase.js

--- chatify-master/frontend/src/firebase.js
+++ chatify-master/frontend/src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDvRdC0yDjO4mOVdAidpf1cuhj8wZCF344",
   authDomain: "quatify-auth.firebaseapp.com",
@@ -14,7 +11,5 @@ const firebaseConfig = {
   measurementId: "G-LM02ZKCMLN"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
